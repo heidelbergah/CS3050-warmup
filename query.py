522be26059c9fdb80cfa9f20a656f34b6234a2b7
Benjamin Quackenbush <bquacken@uvm.edu>
2025-01-31 20:05:12 -0500

    get_input() completed with help prompt (some testing done. more needed)

--- query.py
+++ query.py
@@ -19,6 +19,128 @@ BUGS
 
 """
 
+def get_input():
+    valid_input = False
+    categories = ["manufacturer","type","rating","shelf","potassium"]
+    manufacturer_options = ["American Home Food Products","General Mills",
+                            "Kelloggs","Nabisco","Post","Quaker Oats",
+                            "Ralston Purina"]
+    while not valid_input:
+        #initial prompt for input 
+        user_input = input("Please enter your query if you need help type \'help\'\n>>")
+        print("\n")#for output readability
+
+
+        if user_input == "help":
+            print("Query Language:\nmanufacuter: manufacturer of the cereal\n" + 
+                  "type: type of cereal, cold or hot\n" + 
+                  "rating: rating of cereal 0-100\n" + 
+                  "shelf: shelf from the floor 1,2,3\n" + 
+                  "potassium: amount of potassium in cereal\n" + 
+                  "and: and\nor: or\n<: less than\n<=: less than or equal to\n" + 
+                  ">: greater than\n>=: greater than or equal to\n" + 
+                  "==: is or equal to\n(): allows for compound expressions\n" + 
+                  "!: negative operator\n\nExamples:\n" + 
+                  "manufacuter == Kelloggs and potassium > 0\n" + 
+                  "shelf == 3 or potassium > 0\n" + 
+                  "Queries are case sensitive\n")
+        else:
+            valid_input = True
+            #remove '(' and ')' as they interrupt recognition of inputs
+            user_input = user_input.replace('(','')
+            user_input = user_input.replace(')','')
+
+            input_list = user_input.split(' ')
+            error_printing_list = input_list
+            original_index = 0
+
+            #cycles through the list by removing elements until none remain or 
+            #an error is encountered
+            while len(input_list) > 0:
+                if input_list[0] == '':
+                    print("please enter something.\n")
+                    valid_input = False
+                    break
+                if input_list[0] in categories:
+                    #if the current index is a query keyword we first check the index 
+                    # ahead of it to make sure that an operator is being used
+                    if input_list[1] not in ['<','>','<=','>=','==']:
+                        valid_input = False
+                        print("Invalid entry!!")
+                        print(" ".join(error_printing_list[:original_index + 2]) + "<<< error happened here")
+                        print("expected \'<\',\'>\',\'<=\',\'>=\', or \'==\'\n")
+                    else:
+                        #if an operator is used then we check if there is a 
+                        # valid entry for the keyword two indexes ahead
+                        match input_list[0]:
+                            case "manufacturer":
+                                if input_list[2] not in manufacturer_options:
+                                    valid_input = False
+                            case "type":
+                                if input_list[2] not in ["hot","cold"]:
+                                    valid_input = False
+                            case "rating":
+                                try:
+                                    if int(input_list[2]) not in range(101):
+                                        valid_input = False
+                                except:
+                                    print("Invalid data!!!")
+                                    valid_input = False
+                            case "shelf":
+                                try:
+                                    if int(input_list[2]) not in range(1,4):
+                                        valid_input = False
+                                except:
+                                    print("Invalid data!!!")
+                                    valid_input = False
+                            case "potassium":
+                                try:
+                                    if int(input_list[2]) not in range(1,1001):
+                                        valid_input = False
+                                except:
+                                    print("Invalid data!!!")
+                                    valid_input = False
+                            case _:
+                                valid_input = False
+                        
+                        if not valid_input:
+                            print("Invalid entry!!")
+                            print(" ".join(error_printing_list[:original_index + 3]) + " <<< error happened here")
+                            print("data of that type is not accepted or the entry exceeds " +
+                                  "the accepted range of data\n")
+                            break
+                else:
+                    #error message for if a keyword is not used or misspelled 
+                    valid_input = False
+                    print("Invalid entry!!")
+                    print(" ".join(error_printing_list[:original_index]) + " <<< error happened here")
+                    print("expected \'manufacturer\', \'type\', \'rating\', \'shelf\', or \'potassium\'\n")
+                    break
+                
+                #removes the 3 items we just read and made sure were a valid logical
+                #statement
+                input_list = input_list[3:]
+                original_index += 3
+                
+                #checks if there are any more words left in the query
+                if len(input_list) > 0:
+                    #if yes they should always be either 'and', or 'or' if not 
+                    #it throws an error 
+                    if input_list[0] not in ["and","or"]:
+                        print("Invalid entry!!")
+                        print(" ".join(error_printing_list[:original_index + 3]) + " <<< error happened here")
+                        print("expected \'or\' or \'and\'\n")
+                        break
+                    else:
+                        #removes either 'and', or 'or' from the list
+                        #so that any valid query has a keyword and the loop can run again
+                        input_list = input_list[1:]
+                        original_index += 1
+            
+        
+    return
+
+
 def parse_query(input_query):
     parts = input_query.split(' ')
 
@@ -162,5 +284,6 @@ def fancy_print(cereals):
         print(f"- {cereal}")
 
 
-execute_query("shelf == 1 and manufacturer == Nabisco")
+#execute_query("shelf == 1 and manufacturer == Nabisco")
+#get_input()
 
